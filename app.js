9ac70dfe00bffda9385877d0871aad76389ac6fe
None None <adm_sanket_24088@mlops-leader0.sanket-5.kfjr-x0dh.cloudera.site>
2024-08-29 05:18:10 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => res.send('Hello, Kubernetes!'));
+
+app.listen(port, () => console.log(`App running on port ${port}`));
+
